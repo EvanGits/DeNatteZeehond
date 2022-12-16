f49f0af916c5375aa4d68a0acfbb8371f639f049
ramonluteijn <ramon.luteijn@rocdeleijgraaf.nl>
2022-12-16 14:07:21 +0100

    verdere opzet

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DeNatteZeehond
 Oefenproject voor het examen 
 Hallo Berend zit in de github
+Berend hoe weinig kun jij. alleen maar aan het gapen is die jongen
--- required/header.php
+++ required/header.php
@@ -1,11 +1,16 @@
 <!-- user check and navbar button check-->
 <nav class="navbar bg-light navbar-expand-md navbar-light">
     <div class="container">
-        <div class="collapse navbar-collapse" id="navmenu">
+
+        <a style="text-decoration:none;" href="<?=ROOT?>">
+            <h2 class="p-2 text-dark">De Natte Zeehond</h2>
+        </a> 
+
+        <div class="collapse navbar-collapse" id="navmenu">    
             <ul class="nav nav-pills nav-fill ms-auto">
                 <?php if($page == "home" || $page == "") : ?>
                     <li>
-                        <a href="<?= ROOT ?>/home" class="nav-link bg-primary text-light fs-5">Home</a>
+                        <a href="<?= ROOT ?>/home" class="nav-link bg-primary    text-light fs-5">Home</a>
                     </li>
                 <?php else : ?>
                     <li>
@@ -14,6 +19,20 @@
                 <?php endif; ?>
             </ul>
         </div> 
+        <div class="collapse navbar-collapse" id="navmenu">    
+            <ul class="nav nav-pills nav-fill ms-auto">
+                <?php if($page == "Doneren") : ?>
+                    <li>
+                        <a href="<?= ROOT ?>/donation" class="nav-link bg-primary text-light fs-5">Doneren</a>
+                    </li>
+                <?php else : ?>
+                    <li>
+                        <a href="<?= ROOT ?>/donation" class="nav-link text-primary border border-primary border-1 rounded fs-5">Doneren</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div> 
+                    
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
             <span class="navbar-toggler-icon"></span>
         </button>
