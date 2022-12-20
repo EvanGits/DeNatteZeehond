9eac43abfe7d3d038f9ba77f53cacb5a82a02d59
Berend305 <74530848+Berend305@users.noreply.github.com>
2022-12-20 10:26:28 +0100

    footer done

---
+++ required/footer.php
@@ -0,0 +1,8 @@
+<footer class="text-center text-lg-start mt-5 fixed-bottom">
+
+<div class="text-center p-3">
+    <p>&copy; 2022 | <a href="./" class="text-decoration-none"><span class="text-dark">De Natte Zeehond </span></span></p></a>
+    <p><a href="./" class="text-decoration-none"><span class="text-dark"> <img src="./images/insta.png" width="35vh" height="35vh"> <img src="./images/facebook.png" width="35vh" height="35vh"> <img src="./images/linkedin.png" width="35vh" height="35vh"> <img src="./images/tik-tok.png" width="35vh" height="35vh"></span></p></a>
+</div>
+
+</footer>
