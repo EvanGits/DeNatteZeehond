aef2b6b0ba971ff0c64cfe338b19061a232b1559
Berend305 <berend.beerendonk@rocdeleijgraaf.nl>
2022-12-28 02:58:29 +0100

    ticket admin page klaar

--- pages/tickets/addTicket.php
+++ pages/tickets/addTicket.php
@@ -1,20 +1,12 @@
 <?php
-// if(isset($_SESSION["user"]))
-// {
-//         $customer = Customer::getCustomerById($_SESSION["user"]->getId());
-
-//     if(!empty($_POST))
-//     { 
-        
-       
-//     }
-// }
+$customer = $_SESSION["user"];
 $email = Customer::selectCustomerList();
-print_r($_POST["date"]);
 if (!empty($_POST["Toevoegen"])) {
-    //Tickets::insertInToTicket($_POST["date"], $_POST["email"]);
+    Tickets::insertInToTicket($_POST["date"], $_POST["email"]);
     header("Location: ". ROOT . "/donation");
 }
+
+print_r($_SESSION["user"]);
 ?>
 
 <?php if ($_SESSION["user"]->getcustomerStatusId() == 2) :?>
@@ -35,11 +27,10 @@ if (!empty($_POST["Toevoegen"])) {
         </div>
         <div class="mb-3">
             <label class="form-label">Datum</label>
-            <input type="datetime-local" min="<?=date("Y-m-d H:i")?>" 
-            class="form-control fs-4" name="date" required>
+            <input type="datetime-local" min="<?=date("d-m-y H:i")?>" class="form-control fs-4" name="date" required>
         </div>
         <div class="d-flex justify-content-center">
-            <input class="mw-3 btn btn-lg text-light btn-success" type="submit" value="Toevoegen"> 
+            <input class="mw-3 btn btn-lg text-light btn-success" type="submit" name="Toevoegen" value="Toevoegen"> 
         </div>
         <div class="text-center mt-3">
                 <h5 class="text-danger">
@@ -48,34 +39,28 @@ if (!empty($_POST["Toevoegen"])) {
         </div>
     </form>
 </div>
-    <?php elseif ($_SESSION["user"]->getcustomerStatusId() == 1) :?>
-        <div class="d-flex justify-content-center mt-5">
-        <form class="form-horizontal p-5 rounded primary-box-color shadow" method="post">
-            <div class="mb-3">
-                <label class="form-label">Naam:</label>
-                <input class="form-control fs-4" type="text" name="name" value="<?=ucwords($customer->getName())?>" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Email:</label>
-                <input class="form-control fs-4" type="email" name="email" value="<?=$customer->getEmail()?>" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Telefoonnummer:</label>
-                <input class="form-control fs-4" type="text" name="phone" value="<?=$customer->getPhone()?>" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Datum</label>
-                <input type="datetime-local" min="<?=date("Y-m-d H:i")?>" 
-                class="form-control fs-4" name="date" required>
-            </div>
-            <div class="d-flex justify-content-center">
-                <input class="mw-3 btn btn-lg text-light btn-success" type="submit" value="Toevoegen"> 
-            </div>
-            <div class="text-center mt-3">
-                    <h5 class="text-danger">
-                        <?php if(!empty($error)) {echo $error;}?>
-                    </h5>
-            </div>
-        </form>
-    </div>
-<?php endif;?>
+<?php elseif ($_SESSION["user"]->getcustomerStatusId() == 1) :?>
+<section class="text-center mt-5">
+    <h2>Ticket kopen </h2>
+</section>
+<div class="d-flex justify-content-center mt-5">
+    <form class="form-horizontal p-5 rounded primary-box-color shadow" method="post">
+        <div class="mb-3">
+        <label class="form-label">Email:</label>
+                <input class="form-control fs-4" type="email" name="email" value="<?=$customer->getEmail()?>" required>       
+        </div>
+        <div class="mb-3">
+        <label class="form-label">Datum</label>
+            <input type="datetime-local" min="<?=date("d-m-y H:i")?>" class="form-control fs-4" name="date" required>
+        </div>
+        <div class="d-flex justify-content-center">
+            <input class="mw-3 btn btn-lg text-light btn-success" type="submit" name="Toevoegen" value="Toevoegen"> 
+        </div>
+        <div class="text-center mt-3">
+                <h5 class="text-danger">
+                    <?php if(!empty($error)) {echo $error;}?>
+                </h5>
+        </div>
+    </form>
+</div>
+<?php endif; ?>
